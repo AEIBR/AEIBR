3af0d2e1105002d72598df91fcb176de9a4c862b
A E I B R <AEIBR@users.noreply.github.com>
2016-06-16 13:20:09 +0530

    Initial commit
    verify.sh, to --/verify/-- the builds after completion of "Grunt-Task(s)", for "AEIBR®".
    Usage: $ sh verify.sh

---
+++ verify.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+# =============================== AEIBR® ===============================
+#               Albert Einstein Institute of Bond Research
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#  Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ======================================================================
