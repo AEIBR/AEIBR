9fea05450b004f2b7a085e3ff761860338271089
A E I B R <AEIBR@users.noreply.github.com>
2016-06-21 18:58:19 +0530

    Initial commit
    Markdown 1.0.1 (18 KB) — 17 Dec 2004.
    Markdown is a text-to-HTML conversion tool for web writers.
    Markdown allows you to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML).
    Help: http://daringfireball.net/projects/markdown/.

---
+++ markdown.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+
+# A E I B R®: Albert Einstein Institute of Bond Research.
+# ----------------------------------------------------------------
+# Author    : Prabhat Kumar [http://prabhatkumar.org/].
+# Copyright : Sequømics Corporation [http://sequomics.com/].
+# ----------------------------------------------------------------
